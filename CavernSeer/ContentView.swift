75668c0dc189fc257a8291746028eebf222ccf6e
Samuel K. Grush <Samuel@Grush.org>
2020-06-27 17:33:46 -0500

    Initial Commit

---
+++ CavernSeer/ContentView.swift
@@ -0,0 +1,42 @@
+//
+//  ContentView.swift
+//  CavernSeer
+//
+//  Created by Samuel Grush on 6/27/20.
+//  Copyright © 2020 Samuel K. Grush. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var selection = 0
+ 
+    var body: some View {
+        TabView(selection: $selection){
+            Text("First View")
+                .font(.title)
+                .tabItem {
+                    VStack {
+                        Image("first")
+                        Text("First")
+                    }
+                }
+                .tag(0)
+            Text("Second View")
+                .font(.title)
+                .tabItem {
+                    VStack {
+                        Image("second")
+                        Text("Second")
+                    }
+                }
+                .tag(1)
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
